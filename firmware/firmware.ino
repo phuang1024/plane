92b09a2960fd47ab08b7054b1632163058df9a3b
Patrick Huang <huangpatrick16777216@gmail.com>
2022-09-05 18:31:58 -0700

    Finally motor works.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,66 +1,18 @@
 #include <RH_ASK.h>
+#include <ServoTimer2.h>
 
-using ull = unsigned long long;
+constexpr int PULSE_MIN = 700;
+constexpr int PULSE_MAX = 2300;
 
+RH_ASK _radio;
 
-/**
- * Time in us.
- */
-ull time() {
-    return 1000*millis() + micros()%1000;
-}
-
-
-/**
- * Servo motor that doesn't need the hardware clock thing.
- * After doing motor.write(angle), call motor.update() very frequently so
- * the pulse is correct.
- */
-class ServoMotor {
-private:
-    int pin;
-    // us
-    int pulse_width;
-
-public:
-    const int min_width = 700;
-    const int max_width = 2500;
-
-    ServoMotor() {
-    }
-
-    void attach(int pin) {
-        this->pin = pin;
-    }
-
-    void write(int angle) {
-        pulse_width = map(angle, 0, 180, min_width, max_width);
-        pulse_width = constrain(pulse_width, min_width, max_width);
-    }
-
-    void update() {
-        /*
-        const int t = time() % period;
-        if (t < pulse_width) {
-            digitalWrite(pin, HIGH);
-        } else {
-            digitalWrite(pin, LOW);
-        }
-        */
-        digitalWrite(pin, HIGH);
-        delayMicroseconds(pulse_width);
-        digitalWrite(pin, LOW);
-    }
-};
-
-
-// motors, right_aileron, left_aileron, right_elevator, left_elevator
-ServoMotor _motors[6];
+// right_motor, left_motor, right_aileron, left_aileron, right_elevator, left_elevator
+ServoTimer2 _motors[6];
 constexpr int _motor_pins[6] = {3, 4, 5, 6, 7, 8};
 // min (0, 180), max (0, 180), standard (0, 1000)
 constexpr int _motor_limits[6][3] = {
-    {50, 180, 0},
-    {50, 180, 0},
+    {30, 180, 0},
+    {30, 180, 0},
     {180, 50, 300},
     {20, 160, 300},
     {0, 180, 500},
@@ -69,85 +21,72 @@ constexpr int _motor_limits[6][3] = {
 
 
 /**
- * Call update on all motors for specified time.
- */
-void update_and_delay(int ms) {
-    const int start = millis();
-    while (millis() - start < ms) {
-        for (int i = 0; i < 6; i++) {
-            _motors[i].update();
-        }
-        delay(19);
-    }
-}
-
-
-/**
  * @param value  0 to 1000 interpolating between min and max
  */
 void set_motor(int motor, int value) {
     int down = _motor_limits[motor][0];
     int up = _motor_limits[motor][1];
-    int v = map(value, 0, 1000, down, up);
+    int degree = map(value, 0, 1000, down, up);
+    int pulse = map(degree, 0, 180, PULSE_MIN, PULSE_MAX);
+    pulse = constrain(pulse, PULSE_MIN, PULSE_MAX);
 
-    int min, max;
-    if (down < up) {
-        min = down;
-        max = up;
-    } else {
-        min = up;
-        max = down;
-    }
-    v = constrain(v, min, max);
-
-    _motors[motor].write(v);
+    _motors[motor].write(pulse);
 }
 
 
 void test_servos() {
     for (int i = 2; i < 6; i++)
         set_motor(i, 0);
-    update_and_delay(1000);
+    delay(1000);
     for (int i = 2; i < 6; i++)
         set_motor(i, 1000);
-    update_and_delay(1000);
+    delay(1000);
     for (int i = 2; i < 6; i++)
         set_motor(i, _motor_limits[i][2]);
-    update_and_delay(1000);
+    delay(1000);
 }
 
 void test_motors() {
     set_motor(0, 100);
     set_motor(1, 100);
-    update_and_delay(100);
+    delay(100);
     set_motor(0, 0);
     set_motor(1, 0);
-    update_and_delay(1000);
+    delay(1000);
 }
 
 void setup() {
-    Serial.begin(9600);
+    //Serial.begin(9600);
+
+    if (!_radio.init())
+        Serial.println("radio init failed");
 
     for (int i = 0; i < 5; i++) {
         _motors[i].attach(_motor_pins[i]);
         set_motor(i, _motor_limits[i][2]);
     }
 
-    set_motor(0, 1000);
-    set_motor(1, 1000);
-    set_motor(2, 1000);
-    update_and_delay(6000);
-    set_motor(0, 0);
-    set_motor(1, 0);
-    set_motor(2, 0);
-    update_and_delay(6000);
-    return;
-
     delay(8000);
     test_servos();
     test_motors();
+
+    set_motor(0, 100);
+    set_motor(1, 100);
 }
 
 
 void loop() {
+    uint8_t buf[12];
+    uint8_t buflen = sizeof(buf);
+    if (_radio.recv(buf, &buflen)) // Non-blocking
+    {
+      int i;
+      // Message with a good checksum received, dump it.
+      //Serial.print("Message: ");
+      //Serial.println((char*)buf);         
+      set_motor(2, 1000);
+      delay(200);
+      set_motor(2, 0);
+      delay(200);
+    }
 }
