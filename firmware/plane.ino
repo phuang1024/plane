ec1b81b6b9678bc26464f31935c3fab7697d9086
Patrick Huang <huangpatrick16777216@gmail.com>
2022-09-07 16:35:31 -0700

    Range driven controls.

--- firmware/plane.ino
+++ firmware/plane.ino
@@ -9,25 +9,27 @@ RH_ASK _radio;
 // right_motor, left_motor, right_aileron, left_aileron, right_elevator, left_elevator
 ServoTimer2 _motors[6];
 constexpr int _motor_pins[6] = {3, 4, 5, 6, 7, 8};
-// min (0, 180), max (0, 180), standard (0, 1000)
+// standard (0, 1000), range unidirectional (0, 1000)
 constexpr int _motor_limits[6][3] = {
-    {30, 180, 0},
-    {30, 180, 0},
-    {180, 50, 300},
-    {20, 160, 300},
-    {0, 180, 500},
-    {180, 0, 500}
+    {0, 1000},
+    {0, 1000},
+    {600, 400},
+    {400, -400},
+    {500, 400},
+    {500, 400},
 };
 
 
 /**
- * @param value  0 to 1000 interpolating between min and max
+ * @param value  -1000 to 1000 interpolating between min and max
  */
 void set_motor(int motor, int value) {
-    int down = _motor_limits[motor][0];
-    int up = _motor_limits[motor][1];
-    int degree = map(value, 0, 1000, down, up);
-    int pulse = map(degree, 0, 180, PULSE_MIN, PULSE_MAX);
+    int std = _motor_limits[motor][0];
+    int range = _motor_limits[motor][1];
+    int up = std + range;
+    int down = std - range;
+    int fac = map(value, -1000, 1000, down, up);
+    int pulse = map(fac, 0, 1000, PULSE_MIN, PULSE_MAX);
     pulse = constrain(pulse, PULSE_MIN, PULSE_MAX);
 
     _motors[motor].write(pulse);
@@ -36,19 +38,19 @@ void set_motor(int motor, int value) {
 
 void test_servos() {
     for (int i = 2; i < 6; i++)
-        set_motor(i, 0);
+        set_motor(i, -1000);
     delay(1000);
     for (int i = 2; i < 6; i++)
         set_motor(i, 1000);
     delay(1000);
     for (int i = 2; i < 6; i++)
-        set_motor(i, _motor_limits[i][2]);
+        set_motor(i, 0);
     delay(1000);
 }
 
 void test_motors() {
-    set_motor(0, 100);
-    set_motor(1, 100);
+    set_motor(0, 150);
+    set_motor(1, 150);
     delay(100);
     set_motor(0, 0);
     set_motor(1, 0);
@@ -63,15 +65,15 @@ void setup() {
 
     for (int i = 0; i < 5; i++) {
         _motors[i].attach(_motor_pins[i]);
-        set_motor(i, _motor_limits[i][2]);
+        set_motor(i, 0);
     }
 
     delay(8000);
     test_servos();
     test_motors();
 
-    set_motor(0, 100);
-    set_motor(1, 100);
+    //set_motor(0, 150);
+    //set_motor(1, 150);
 }
 
 
