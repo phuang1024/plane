8150d1b5727f63796ddedfc2b557e261d72e72f0
Patrick Huang <huangpatrick16777216@gmail.com>
2022-10-01 14:55:29 -0700

    Refactoring.

--- firmware/plane/plane.ino
+++ firmware/plane/plane.ino
@@ -9,33 +9,29 @@
 
 const bool CALIBRATE_MODE = false;
 
+ServoTimer2 motor;
+ServoTimer2 servo;
 
-void setup() {
-    Serial.begin(9600);
-    Wire.begin();
 
+/**
+ * Called if calibrate mode.
+ * Ideally, don't do anything after this.
+ */
+void calibrate() {
+    Serial.println("Calibrate mode.");
+
+    // Calibrate motor (high then low).
+    motor.write(2300);
+    delay(7000);
+    motor.write(700);
+}
+
+
+void mainloop() {
     IMU imu_sensor;
     Baro baro_sensor;
     imu_sensor.init();
     baro_sensor.init();
-
-    ServoTimer2 motor;
-    ServoTimer2 servo;
-    motor.attach(2);
-    servo.attach(3);
-    motor.write(700);
-
-    // Calibrate if calibrate mode.
-    if (CALIBRATE_MODE) {
-        Serial.println("Calibrate mode.");
-
-        // Calibrate motor (high then low).
-        motor.write(2300);
-        delay(7000);
-        motor.write(700);
-
-        while (true);  // Don't do anything else.
-    }
 
     // Find (0, 0, 0) of rotational position.
     // The delay here also makes sure ESC is initialized.
@@ -43,7 +39,41 @@ void setup() {
     get_rot_origin(imu_sensor, rot_origin);
 
     PIDControl pidctrl(3, 0.2, 0.004, rot_origin[0]);
-    LEDBlinker ledblink(13, 50, 1950);
+    LEDBlinker ledblink(13, 50, 2950);
+
+    motor.write(1000);  // TODO testing.
+
+    while (true) {
+        IMURead imu = imu_sensor.read();
+        //BaroRead baro = baro_sensor.read();
+
+        float value = (float)imu.ax;
+        float ctrl = pidctrl.control(value);
+        int angle = constrain(mapf(ctrl, -1, 1, 700, 2300), 700, 2300);
+        Serial.println(angle);
+        servo.write(angle);
+
+        ledblink.update();
+
+        delay(20);
+    }
+}
+
+
+void setup() {
+    Serial.begin(9600);
+    Wire.begin();
+
+    motor.attach(2);
+    motor.write(700);   // off
+    servo.attach(3);
+
+    if (CALIBRATE_MODE) {
+        calibrate();
+        return;
+    }
+
+    mainloop();
 
     // TODO temporary radio test
     /*
@@ -70,22 +100,6 @@ void setup() {
     }
     */
 
-    motor.write(1000);
-
-    while (true) {
-        IMURead imu = imu_sensor.read();
-        //BaroRead baro = baro_sensor.read();
-
-        float value = (float)imu.ax;
-        float ctrl = pidctrl.control(value);
-        int angle = constrain(mapf(ctrl, -1, 1, 700, 2300), 700, 2300);
-        Serial.println(angle);
-        servo.write(angle);
-
-        ledblink.update();
-
-        delay(20);
-    }
 }
 
 
