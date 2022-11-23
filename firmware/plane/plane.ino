900c353bc0bccb91e5ecc059ad73f47b56f16a0f
Patrick Huang <huangpatrick16777216@gmail.com>
2022-11-23 13:26:37 -0800

    Added elev control.

--- firmware/plane/plane.ino
+++ firmware/plane/plane.ino
@@ -11,13 +11,13 @@
 const bool CALIBRATE_MODE = false;
 
 ServoTimer2 _engine;
-// flap, ailr
-const int _scnt = 2;
+// flap, ailr, aill, elev
+const int _scnt = 4;
 ServoTimer2 _servos[_scnt];
-int _servo_pin[_scnt] = {4, 7};
+int _servo_pin[_scnt] = {4, 7, 3, 6};
 // pulse widths
-int _servo_mid[_scnt] = {1400, 1190};
-int _servo_range[_scnt] = {400, 370};
+int _servo_mid[_scnt] = {1400, 1190, 1190, 1650};
+int _servo_range[_scnt] = {380, 350, -365, 450};
 
 
 /**
@@ -54,42 +54,47 @@ void mainloop() {
 
     // Find (0, 0, 0) of rotational position.
     // The delay here also makes sure ESC is initialized.
-    /*
     float rot_origin[3];
     get_rot_origin(imu_sensor, rot_origin);
 
-    PIDControl pidctrl(3, 0.2, 0.004, rot_origin[0]);
     LEDBlinker ledblink(13, 50, 2950);
-    */
+    PIDControl pid_roll(2, 0.15, 0.005, rot_origin[0]),
+               pid_pitch(2, 0.15, 0.005, rot_origin[1]);
 
     //motor.write(1000);  // TODO testing.
 
     // TODO test
+    /*
     while (true) {
         for (int pos = -1000; pos <= 1000; pos += 500) {
-            write_servo(0, pos);
-            write_servo(1, pos);
+            write_servo(3, pos);
             delay(500);
         }
     }
+    */
 
     // TODO PID test
-    /*
     while (true) {
         IMURead imu = imu_sensor.read();
         //BaroRead baro = baro_sensor.read();
 
+        // PID roll
         float value = (float)imu.ax;
-        float ctrl = pidctrl.control(value);
-        int angle = constrain(mapf(ctrl, -1, 1, 700, 2300), 700, 2300);
-        Serial.println(angle);
-        servo.write(angle);
+        float ctrl = pid_roll.control(value);
+        ctrl = constrainf(ctrl, -1, 1);
+        write_servo(1, 1000*ctrl);
+        write_servo(2, -1000*ctrl);
+
+        // PID pitch
+        value = (float)imu.ay;
+        ctrl = pid_pitch.control(value);
+        ctrl = constrainf(ctrl, -1, 1);
+        write_servo(3, -1000*ctrl);
 
         ledblink.update();
 
         delay(20);
     }
-    */
 }
 
 
@@ -103,11 +108,26 @@ void setup() {
         _servos[i].attach(_servo_pin[i]);
         write_servo(i, 0);
     }
+    delay(1000);
 
     if (CALIBRATE_MODE) {
         calibrate();
         return;
     }
+
+    // Test servos
+    for (int i = 0; i < _scnt; i++) {
+        write_servo(i, -1000);
+    }
+    delay(1000);
+    for (int i = 0; i < _scnt; i++) {
+        write_servo(i, 1000);
+    }
+    delay(1000);
+    for (int i = 0; i < _scnt; i++) {
+        write_servo(i, 0);
+    }
+    delay(1000);
 
     mainloop();
 
